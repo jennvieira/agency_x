612d60a3de980da6354e469a902c09b4f61ddfdc
carmarsh <c_marsh5@fanshaweonline.ca>
2017-02-01 18:00:05 -0500

    updated build files

--- script/main.js
+++ script/main.js
@@ -3,6 +3,23 @@
 "use strict";
 console.log("Seaf fired");
 
+var nav_links = document.querySelectorAll('.nav-link'),
+    section1content = document.querySelectorAll('.section_one_content');
+
+
+function changeContent(){
+
+    [].forEach.call(section1content, function(section){
+      section.style.display="none";
+    });
+
+    section1content[this.id].style.display="table-cell";
+}
+
+
+[].forEach.call(nav_links, function(nav_link) {
+  nav_link.addEventListener("click", changeContent, false);
+});
 
 
 })();
